f9575902dc4c33d7990c73eebb59d0f9c47450a8
shaobo <zsb940680883@outlook.com>
2021-12-30 21:35:01 +0800

    ssh

--- apt_install.sh
+++ apt_install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo apt update
 sudo apt upgrade
-sudo apt install build-essential cmake curl git neofetch proxychains fcitx fish
+sudo apt install build-essential cmake curl git neofetch proxychains fcitx fish openssh-server net-tools
 sudo chsh -s /usr/bin/fish 
 echo "socks 127.0.0.1 7891" >> /etc/proxychains.conf
 echo "http  127.0.0.1 7890" >> /etc/proxychains.conf
